aff1e47bf9140f98c33c023c33db0521a7366cfa
tatobrito <gnothisway@hotmail.com>
2024-07-05 16:50:56 -0300

    criando a rota de index e a show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_01_194310) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_01_203442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_01_194310) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "kind"
   end
 
 end
